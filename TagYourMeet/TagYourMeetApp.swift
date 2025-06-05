63859c564f0ab58b33877b57fe67cdd7da25fe92
jacklr6 <captapplesauce6@gmail.com>
2025-06-05 16:00:16 -0400

    Initial Commit

---
+++ TagYourMeet/TagYourMeetApp.swift
@@ -0,0 +1,17 @@
+//
+//  TagYourMeetApp.swift
+//  TagYourMeet
+//
+//  Created by Jack Rogers on 6/5/25.
+//
+
+import SwiftUI
+
+@main
+struct TagYourMeetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
